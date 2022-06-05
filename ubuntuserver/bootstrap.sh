323769b1eadb8ede1bd54110f06000b4a5190616
Filipe Freire <livrofubia@gmail.com>
2022-06-05 15:51:53 +0100

    add ubuntu server 22.04 vagrantfile

---
+++ ubuntuserver/bootstrap.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Bootstrap machine
+
+export DEBIAN_FRONTEND=noninteractive
+
+ensure_netplan_apply() {
+    # First node up assign dhcp IP for eth1, not base on netplan yml
+    sleep 5
+    sudo netplan apply
+}
+
+step=1
+step() {
+    echo "Step $step $1"
+    step=$((step+1))
+}
+
+resolve_dns() {
+    step "===== Create symlink to /run/systemd/resolve/resolv.conf ====="
+    sudo rm /etc/resolv.conf
+    sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+}
+
+install_docker() {
+    step "===== Installing docker ====="
+    sudo apt update
+    sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt update
+    sudo apt install -y docker docker.io
+    sudo groupadd docker
+    sudo gpasswd -a $USER docker
+    sudo chmod 777 /var/run/docker.sock
+    # Add vagrant to docker group
+    sudo groupadd docker
+    sudo gpasswd -a vagrant docker
+    sudo systemctl restart docker
+}
+
+install_updates() {
+    step "===== Installing updates ====="
+    sudo apt update
+    sudo apt -y upgrade
+}
+
+install_openssh() {
+    step "===== Installing openssh ====="
+    sudo apt update
+    sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt install -y openssh-server
+    sudo systemctl enable ssh
+}
+
+# install_tools() {
+#     sudo apt install -y python-pip
+#     sudo apt install -y default-jre
+#         pip install kafka --user
+#         pip install kafka-python --user
+# }
+
+setup_root_login() {
+    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sudo systemctl restart ssh
+    sudo echo "root:rootroot" | chpasswd
+}
+
+setup_welcome_msg() {
+    sudo apt -y install cowsay
+    sudo echo -e "\necho \"Welcome to Vagrant Ubuntu Server 22.04\" | cowsay\n" >> /home/vagrant/.bashrc
+    sudo ln -s /usr/games/cowsay /usr/local/bin/cowsay
+}
+
+main() {
+    ensure_netplan_apply
+    resolve_dns
+    install_updates
+    install_openssh
+    install_docker
+    setup_root_login
+    setup_welcome_msg
+}
+
+main
